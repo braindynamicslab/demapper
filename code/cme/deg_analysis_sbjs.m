05ad549e442294dc939a09b69a03d8e04cf8d89a
Daniel Hasegan <danielhasegan@gmail.com>
2022-03-21 10:44:28 -0700

    print out the stats and change to be compatible to all stat types

--- code/cme/deg_analysis_sbjs.m
+++ code/cme/deg_analysis_sbjs.m
@@ -1,11 +1,20 @@
 %{
-Run the with the following command on Sherlock (on `sdev`)
+## Run command locally:
+
+datafolder = '/Users/dh/workspace/BDL/demapper/data/cme/';
+fn_timing = '/Users/dh/workspace/BDL/neurolens/data/data-cme-shine375/timing.csv';
+output_dir = '/Users/dh/workspace/BDL/demapper/results/cme/test';
+stat_type = 'compute_degrees';
+deg_analysis_sbjs
+
+## Run the with the following command on Sherlock (on `sdev`)
 
 module load matlab
 DATAFOLDER="/scratch/groups/saggar/demapper-cme/mappers_cmev2.json/"
 FN_TIMING="/oak/stanford/groups/saggar/data-cme-shine375/timing.csv"
 OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/mappers_cmev2.json/"
-ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}';"
+STAT_TYPE="compute_degrees"
+ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}'; stat_type='${STAT_TYPE}';"
 matlab -r "${ARGS} run('code/cme/deg_analysis_sbjs.m')"
 
 %}
@@ -21,6 +30,10 @@ end
 if ~exist('output_dir', 'var')
     error('MapperToolbox:IncorrectSetup', ...
         'Please set up variable output_dir');
+end
+if ~exist('stat_type', 'var')
+    error('MapperToolbox:IncorrectSetup', ...
+        'Please set up variable stat_type');
 end
 
 timing_table = readtable(fn_timing, 'FileType', 'text');
@@ -62,6 +75,11 @@ for sbjid = 1:length(sbjs)
 end
 disp('...done')
 
+stat_outdir = fullfile(output_dir, stat_type);
+if ~exist(stat_outdir, 'dir')
+    mkdir(stat_outdir)
+end
+
 fprintf('Processing %d mappers...\n', length(all_mappers));
 for mid = 1:length(all_mappers)
     mapper_name = cell2mat(all_mappers(mid));
@@ -71,24 +89,39 @@ for mid = 1:length(all_mappers)
     for sbjid = 1:length(sbjs)
         sbj = cell2mat(sbjs(sbjid));
 
-        mapper_path = fullfile(datafolder, sbj, mapper_name, 'res.mat');
-        all_degs(sbjid, :) = process(mapper_path);
+        mapper_path = fullfile(datafolder, sbj, mapper_name);
+        all_degs(sbjid, :) = process(mapper_path, stat_type);
     end
 
     avg_degs = mean(all_degs, 1);
-    output_path = fullfile(output_dir, [mapper_name, '.png']);
+    output_path = fullfile(stat_outdir, [mapper_name, '.png']);
     plot_degs(avg_degs, timing_labels, timing_changes, mapper_name, output_path);
+
+    stat_output_path = fullfile(stat_outdir, ['avgstat_', mapper_name, '.1D']);
+    write_1d(avg_degs, stat_output_path);
 end
 disp('...done')
 
 
 %% Helper functions
-function degs = process(datapath)
-    res = load(datapath).res;
-    
-    K = res.memberMat' * (res.adjacencyMat > 0);
-    degs = sum(K, 2)';
-    degs = normalize(degs, 'Range');
+function degs = process(mapper_path, stat_type)
+    switch stat_type
+        case 'compute_degrees'
+            datapath = fullfile(mapper_path, 'res.mat');
+            res = load(datapath).res;
+            
+            K = res.memberMat' * (res.adjacencyMat > 0);
+            degs = sum(K, 2)';
+            degs = normalize(degs, 'Range');
+        case {'betweenness_centrality_TRs_avg', 'betweenness_centrality_TRs_max', ...
+                'core_periphery_TRs_avg', 'core_periphery_TRs_max', 'degrees_TRs'}
+            datapath = fullfile(mapper_path, ['stats_', stat_type, '.1D']);
+            degs = read_1d(datapath);
+            degs = normalize(degs, 'Range');
+        otherwise
+            error('MapperToolbox:UnrecognizedStatType', ...
+                'Please set up a correct value for `stat_type`');
+    end
 end
 
 function plot_degs(degs, timing_labels, timing_changes, mapper_name, output_path)
@@ -128,4 +161,14 @@ function plot_degs(degs, timing_labels, timing_changes, mapper_name, output_path
 
     saveas(f, output_path);
     close(f);
+end
+
+function data = read_1d(data_path)
+data_opts = delimitedTextImportOptions('Delimiter', ' ');
+data = readmatrix(data_path, data_opts);
+data = str2double(data);
+end
+
+function write_1d(mat_obj, data_path)
+writematrix(mat_obj, data_path, 'Delimiter', 'space', 'FileType', 'text')
 end
