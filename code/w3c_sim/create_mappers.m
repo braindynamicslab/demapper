b355243ed387998cea44db78636e65cd329a791c
Daniel Hasegan <danielhasegan@gmail.com>
2022-03-28 16:44:59 -0700

    Add analysis for W3C

---
+++ code/w3c_sim/create_mappers.m
@@ -0,0 +1,8 @@
+cohort_csv = '/Users/dh/workspace/BDL/demapper/data/w3c_sim/cohort_local.csv';
+data_root = '/Users/dh/workspace/BDL/demapper/data/w3c_sim/';
+config_path = '/Users/dh/workspace/BDL/demapper/code/configs/mappers_w3c.json';
+output_dir = '/Users/dh/workspace/BDL/demapper/results/w3c_sim/mappers/';
+
+poolsize = 4;
+
+run_main
