9c0de03746b176ec3a21fbc513f8de1215cae186
Daniel Hasegan <danielhasegan@gmail.com>
2022-03-30 11:25:48 -0700

    Add w3c circle test

--- code/w3c_sim/create_mappers.m
+++ code/w3c_sim/create_mappers.m
@@ -1,8 +1,19 @@
 cohort_csv = '/Users/dh/workspace/BDL/demapper/data/w3c_sim/cohort_local.csv';
 data_root = '/Users/dh/workspace/BDL/demapper/data/w3c_sim/';
 config_path = '/Users/dh/workspace/BDL/demapper/code/configs/mappers_w3cv1.json';
-output_dir = '/Users/dh/workspace/BDL/demapper/results/w3c_sim/mappers_v3/';
+output_dir = '/Users/dh/workspace/BDL/demapper/results/w3c_sim/mappers_test/';
 
 rerun_uncomputed = 1;
 
 run_main
+
+
+
+% Local
+datafolder = '/Users/dh/workspace/BDL/demapper/results/w3c_sim/mappers_test/';
+fn_timing = '/Users/dh/workspace/BDL/demapper/data/w3c_sim/task_info.csv';
+output_dir = '/Users/dh/workspace/BDL/demapper/results/w3c_sim/analysis/mappers_test/';
+stat_type = 'degrees_TRs';
+CHANGE_POINTS = 7;
+HAS_INSTRUCTIONS = 0;
+deg_analysis_sbjs
--- code/w3c_sim/gen_project.sh
+++ code/w3c_sim/gen_project.sh
@@ -5,7 +5,7 @@ echo """
       "id0": "SBJ99",
       "id1": "", "id2": "",
       "path": "w3c_simulated_bold.npy",
-      "TR": 1.0
+      "TR": 0.72
 }""" > W3C_ids.json
 
 # Generate the project
@@ -16,7 +16,7 @@ python3 neupipe/mapper.py w3c \
     --output-path /scratch/groups/saggar/demapper-w3c/ \
     --project-dir $GROUP_SCRATCH/dh/pipeline/neupipe/projects/w3c/ \
     --mappertoolbox-dir /scratch/groups/saggar/dh/mappertoolbox-matlab/ \
-    --extra-args has_TR=True,RepetitionTime=1.0
+    --extra-args has_TR=True,RepetitionTime=0.72
 
 # Start the job on all configs
 sbatch -p owners /scratch/groups/saggar/dh/pipeline/neupipe/projects/w3c/run_mapper.sbatch \
@@ -26,3 +26,25 @@ sbatch -p owners /scratch/groups/saggar/dh/pipeline/neupipe/projects/w3c/run_map
 sbatch -p owners /scratch/groups/saggar/dh/pipeline/neupipe/projects/w3c/run_mapper.sbatch \
     /home/users/hasegan/demapper/code/configs/mappers_w3cv2.json \
     --rerun_uncomputed
+
+
+
+# `sdev` and then run the following:
+module load matlab
+DATAFOLDER="/scratch/groups/saggar/demapper-w3c/mappers_w3cv2.json/"
+FN_TIMING="/scratch/groups/saggar/demapper-w3c/data/task_info.csv"
+OUTPUT_DIR="/scratch/groups/saggar/demapper-w3c/analysis/mappers_w3cv2.json/"
+STAT_TYPE="degrees_TRs"
+CHANGE_POINTS=7
+HAS_INSTRUCTIONS=0;
+ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}'; stat_type='${STAT_TYPE}';"
+ARGS="$ARGS HAS_INSTRUCTIONS=${HAS_INSTRUCTIONS}; CHANGE_POINTS=${CHANGE_POINTS};"
+matlab -r "${ARGS} run('code/cme/deg_analysis_sbjs.m')"
+
+# Or run the circle test:
+module load matlab
+DATAFOLDER="/scratch/groups/saggar/demapper-w3c/mappers_w3cv2.json/"
+FN_TIMING="/scratch/groups/saggar/demapper-w3c/data/task_info.csv"
+OUTPUT_DIR="/scratch/groups/saggar/demapper-w3c/analysis/mappers_w3cv2.json/"
+ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}';"
+matlab -r "${ARGS} run('code/w3c_sim/circle_test.m')"
