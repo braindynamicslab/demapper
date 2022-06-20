f46f27917a8254be19f9baba54cb2fc2040d8ff0
Daniel Hasegan <danielhasegan@gmail.com>
2022-06-20 15:49:28 -0700

    Add skip_mat for all compute_temp in cme configs

--- code/cme/gen_project.sh
+++ code/cme/gen_project.sh
@@ -1,11 +1,11 @@
 
 # `sdev` and then run the following:
 module load matlab
-DATAFOLDER="/scratch/groups/saggar/demapper-cme/mappers_cmev5.json/"
+DATAFOLDER="/scratch/groups/saggar/demapper-cme/mappers_cmev4.json/"
 FN_TIMING="/oak/stanford/groups/saggar/data-cme-shine375/timing.csv"
-OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/mappers_cmev5.json/"
+OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/ch8_mappers_cmev4.json/"
 STAT_TYPE="compute_degrees_from_TCM" # "compute_degrees"
-CHANGE_POINTS=7
+CHANGE_POINTS=8
 HAS_INSTRUCTIONS=1;
 ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}'; stat_type='${STAT_TYPE}';"
 ARGS="$ARGS HAS_INSTRUCTIONS=${HAS_INSTRUCTIONS}; CHANGE_POINTS=${CHANGE_POINTS};"
