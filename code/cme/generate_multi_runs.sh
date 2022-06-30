7a5097cf19eb041da2b5555dd9fa8e4017d86fdf
Daniel Hasegan <danielhasegan@gmail.com>
2022-06-30 16:26:46 -0700

    Parameter ifx

--- code/cme/generate_multi_runs.sh
+++ code/cme/generate_multi_runs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE=commands_to_run_cmev3-v4_ch11.m
+CHNR=10
+FILE=commands_to_run_cmev3-v4_ch${CHNR}.m
 
 echo '% starting ' > $FILE
 
@@ -8,17 +9,11 @@ for CONF in mappers_cmev3_fast.json mappers_cmev3.json \
             mappers_cmev4_fast.json mappers_cmev4.json \
             mappers_cmev4_euc_fast.json mappers_cmev4_euc.json
 do
-
-  DATAFOLDER="/scratch/groups/saggar/demapper-w3c/${CONF}/"
-  COHORT_PATH="/scratch/groups/saggar/demapper-w3c/data_subsampled/cohort_shorter.csv"
-  OUTPUT_DIR="/scratch/groups/saggar/demapper-w3c/analysis/${CONF}/"
-  ARGS="datafolder='${DATAFOLDER}'; cohort_path='${COHORT_PATH}'; output_dir='${OUTPUT_DIR}';"
-
   DATAFOLDER="/scratch/groups/saggar/demapper-cme/${CONF}/"
   FN_TIMING="/oak/stanford/groups/saggar/data-cme-shine375/timing.csv"
-  OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/ch11_${CONF}/"
+  OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/ch${CHNR}_${CONF}/"
   STAT_TYPE="degrees_TRs" # "compute_degrees_from_TCM", "compute_degrees"
-  CHANGE_POINTS=11
+  CHANGE_POINTS=${CHNR}
   HAS_INSTRUCTIONS=1;
   ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}'; stat_type='${STAT_TYPE}';"
   ARGS="$ARGS HAS_INSTRUCTIONS=${HAS_INSTRUCTIONS}; CHANGE_POINTS=${CHANGE_POINTS};"
