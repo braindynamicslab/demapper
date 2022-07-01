d2a52857c87fcad7ce3730051160b8b319076fc1
Daniel Hasegan <danielhasegan@gmail.com>
2022-07-01 11:30:01 -0700

    Fix generate multi script

--- code/cme/generate_multi_runs.sh
+++ code/cme/generate_multi_runs.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-CHNR=10
+for CHNR in 7 8 10 11
+do
 FILE=commands_to_run_cmev3v4_ch${CHNR}.m
-
 echo '% starting ' > $FILE
-
-for CONF in mappers_cmev3_fast.json mappers_cmev3.json \
-            mappers_cmev4_fast.json mappers_cmev4.json \
+for CONF in mappers_cmev3_fast.json \
+            mappers_cmev4_fast.json \
             mappers_cmev4_euc_fast.json mappers_cmev4_euc.json
 do
   DATAFOLDER="/scratch/groups/saggar/demapper-cme/${CONF}/"
   FN_TIMING="/oak/stanford/groups/saggar/data-cme-shine375/timing.csv"
   OUTPUT_DIR="/scratch/groups/saggar/demapper-cme/analysis/ch${CHNR}_${CONF}/"
-  STAT_TYPE="degrees_TRs" # "compute_degrees_from_TCM", "compute_degrees"
+  STAT_TYPE="compute_degrees_from_TCM" # "compute_degrees_from_TCM", "compute_degrees", "degrees_TRs"
   CHANGE_POINTS=${CHNR}
   HAS_INSTRUCTIONS=1;
   ARGS="datafolder='${DATAFOLDER}'; fn_timing='${FN_TIMING}'; output_dir='${OUTPUT_DIR}'; stat_type='${STAT_TYPE}';"
@@ -20,4 +19,5 @@ do
   echo "clear" >> $FILE
   echo "${ARGS} run('code/cme/deg_analysis_sbjs.m')" >> $FILE
 
+done
 done
